4f1f9d86642c11c42f2c1fded58a606086257700
Kevin McWhirter <klmcw@yahoo.com>
2024-03-06 10:35:54 -0800

    Generalize approach to finding sys/device dirs to remove

--- envycontrol.py
+++ envycontrol.py
@@ -704,17 +704,38 @@ class CachedConfig:
         logging.debug(f'Created {CACHE_FILE_PATH}')
 
     def write_no_nvidia(self):
-        # see find /sys/devices -name '0000:01:00*'
+        # see find /sys/devices -type d -name '0000:01:00*'
+        # 'w /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/remove - - - - 1\n',
+        # 'w /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.1/remove - - - - 1\n',
         tmpfile_content = [
-            'w /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.0/remove - - - - 1\n',
-            'w /sys/devices/pci0000:00/0000:00:01.0/0000:01:00.1/remove - - - - 1\n',
             'd /run/no-nvidia 0755 klmcw klmcw\n',
             'f /run/no-nvidia/in-effect 0644 klmcw klmcw - 1\n'
-            '\n'
         ]
+
+        for id in find_nvidia_ids_from_lspci():
+            # logging.debug(f'Processing nvidia id={id}')
+            for dir in find_nvidia_sys_device_dirs(id):
+                tmpfile_content.append(f'w {dir}/remove - - - - 1\n')
+
+        tmpfile_content.append('\n')
+
         with open(CachedConfig.NO_NVIDIA_TMPFILE, 'w') as f:
             f.writelines(tmpfile_content)
         logging.debug(f'Created {CachedConfig.NO_NVIDIA_TMPFILE}')
+
+
+def find_nvidia_sys_device_dirs(nvidia_lspci_id):
+    output = subprocess.check_output(['find', '/sys/devices', '-type', 'd', '-name', f'{nvidia_lspci_id}.*']).decode('utf-8')
+    dirs = [dir for dir in output.splitlines() if 'virtual' not in dir]
+    return dirs
+
+
+def find_nvidia_ids_from_lspci():
+    lspci_output = subprocess.check_output(['lspci']).decode('utf-8')
+    ids = {line.split('.')[0]
+           for line in lspci_output.splitlines()
+           if 'NVIDIA' in line}
+    return ids
 
 
 def get_current_mode():
